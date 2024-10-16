c960b635a266a6025024a1d76a025de0eaaa87a1
erasmus-mc-admin <naciyeaydinnn@gmail.com>
2024-10-16 22:04:02 +0200

    my new codes

---
+++ src/test/java/d23oop_collections/AbstractChild.java
@@ -0,0 +1,9 @@
+package d23oop_collections;
+
+public abstract class AbstractChild extends Honda{
+
+
+
+    //Abstract child ,abstract parentin metotlarini implement etmek
+    //zorunda degildir.
+}
